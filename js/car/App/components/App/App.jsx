63a0817e83fe7bd07ac35acd014aca3e4051aac5
Anton Gasperak <anton.gasperak@m2ms.sk>
2022-02-09 08:50:41 +0100

    Remove unneeded stuff

--- js/car/App/components/App/App.jsx
+++ js/car/App/components/App/App.jsx
@@ -1,11 +1,7 @@
-import React, { useState } from 'react';
-import { Helmet } from 'react-helmet';
+import React from 'react';
 
 import { QueryClient, QueryClientProvider } from 'react-query';
-import { loadRDKit } from '../../../RDKit/RDKitFunctions';
 import Layout from '../../../Layout';
-
-loadRDKit();
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -16,42 +12,8 @@ const queryClient = new QueryClient({
 });
 
 export const App = () => {
-  const [projectId, setProjectId] = useState(0);
-
-  function changeProject(projectid) {
-    setProjectId(projectid);
-  }
-
-  function deleteProject() {
-    setProjectId();
-  }
-
-  function generateProtocol() {
-    setProjectId(0);
-    // Need to figure out how to setshow Body vs Prot generator -> setShow in state did not work...
-  }
-
   return (
     <>
-      <Helmet>
-        <link rel="stylesheet" href="https://bootswatch.com/4/sandstone/bootstrap.min.css" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
-        <script
-          src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-          integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
-          crossorigin="anonymous"
-        ></script>
-        <script
-          src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
-          integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN"
-          crossorigin="anonymous"
-        ></script>
-        <script
-          src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js"
-          integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s"
-          crossorigin="anonymous"
-        ></script>
-      </Helmet>
       <QueryClientProvider client={queryClient}>
         <Layout />
       </QueryClientProvider>
